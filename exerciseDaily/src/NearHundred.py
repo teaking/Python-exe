f3c17d7399f99f4f9e0f7ec790a90089d2bdd166
kim <4work.aber@gmail.com>
2018-04-22 01:40:55 +0100

    removed unecessary files

--- exerciseDaily/src/NearHundred.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on 2 Feb 2015
-
-@author: cuddlyteddy
-'''
-
-y = input("Enter the number")
-y = int(y) + 2
-print (y)
--- exerciseDaily/src/ascii.py
+++
@@ -1,7 +0,0 @@
-'''
-Created on 22 Feb 2015
-
-@author: cuddlyteddy
-'''
-a ="y"
-print (ord(a))
